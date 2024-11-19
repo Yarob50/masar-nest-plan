a1b104cbf52411a8d7162a9d6291a7f76a0eee88
Yarob Al Mostafa (يعرُب) <yarob.hm@gmail.com>
2024-11-19 15:39:11 +0300

    up

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { RedisService } from './redis/redis.service';
 import { CustomerModule } from './customer/customer.module';
+import { ConfigModule } from '@nestjs/config';
 
 const entitiesPath = __dirname + '/**/*.entity{.ts,.js}';
 console.log(entitiesPath);
 @Module({
   imports: [
+    // ConfigM
     // TypeOrmModule.forRoot({
     //   type: 'postgres',
     //   host: 'localhost',
@@ -23,13 +25,16 @@ console.log(entitiesPath);
     //   logging: false,
     // }),
 
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.DB_HOST,
       port: 5432,
-      username: 'postgres',
-      password: '',
-      database: 'masar-test-db',
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       entities: [entitiesPath],
       synchronize: true,
